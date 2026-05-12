3b177ce61425d2fc8b865abac08c8f684b64c89e
dediifri2412-dotcom <dediifri2412@gmail.com>
2026-05-12 14:15:00 +0700

    update css styling

--- jscp/settings.js
+++ jscp/settings.js
@@ -9,7 +9,7 @@ let settings = {
     matrixText: 'HAPPYBIRTHDAY',
     matrixColor1: '#ff69b4', // Màu pink mẫu
     matrixColor2: '#ff1493', // Màu pink mẫu
-    sequence: 'HAPPY|BIRTHDAY|TO|WINDA|❤',
+    sequence: 'HAPPY|BIRTHDAY|TO|TIKA|❤',
     sequenceColor: '#ff69b4', // Màu pink mẫu
     gift: './image/happy3.gif',
     enableBook: true,
@@ -349,7 +349,7 @@ function initializeDefaultSettings() {
         matrixText: 'HAPPYBIRTHDAY',
         matrixColor1: '#ff69b4', // Màu pink mẫu
         matrixColor2: '#ff1493', // Màu pink mẫu
-        sequence: 'HAPPY|BIRTHDAY|TO|WINDA|❤',
+        sequence: 'HAPPY|BIRTHDAY|TO|TIKA|❤',
         sequenceColor: '#ff69b4', // Màu pink mẫu
         gift: './image/happy3.gif',
         enableBook: true,
